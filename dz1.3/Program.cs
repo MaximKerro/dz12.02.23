e3691e61ae099dde5973bab5a18758e0250cb1d5
Maxim <sunforge@yandex.ru>
2023-02-12 13:20:46 +0300

    1.3

---
+++ dz1.3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. 22 3 9 -> 22
+Console.WriteLine("Даны 3 числа (22, 3, 9)");
+
+int numberA = 22;
+int numberB = 3;
+int numberC = 9;
+
+int max = numberA;
+int min = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.Write("Максимально число: ");
+Console.WriteLine(max);
