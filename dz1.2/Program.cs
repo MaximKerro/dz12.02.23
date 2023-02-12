0065d241c56129fab3024f21adb59e96e98ce65e
Maxim <sunforge@yandex.ru>
2023-02-12 13:19:24 +0300

    1.2

---
+++ dz1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. 44 5 78 -> 78
+Console.WriteLine("Даны 3 числа (44, 5, 78)");
+
+int numberA = 44;
+int numberB = 5;
+int numberC = 78;
+
+int max = numberA;
+int min = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.Write("Максимально число: ");
+Console.WriteLine(max);
